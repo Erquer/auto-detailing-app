34d1bd9bd0212f4bf51ef0184849143685b2ad89
Erquer <baryla181@gmail.com>
2021-05-25 21:47:18 +0200

    Changes in front: adding material-ui Backend: attempt to connect to database and return data as json.

---
+++ frontend/src/menu/MenuItemLink.tsx
@@ -0,0 +1,23 @@
+import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export interface MenuItemLinkProps {
+    icon: JSX.Element;
+    primary: string;
+    to: string;
+}
+
+export const MenuItemLink = ({icon, primary, to}: MenuItemLinkProps) => {
+    const CustomLink = React.useMemo(() => 
+        React.forwardRef((linkProps, ref) => (
+            <Link to={to} {...linkProps} />
+        )), [to]);
+    
+    return (
+        <ListItem button component={CustomLink}>
+            <ListItemIcon>{icon}</ListItemIcon>
+            <ListItemText primary={primary}/>
+        </ListItem>
+    )
+}
---
+++ frontend/src/utils/colors.ts
@@ -0,0 +1,6 @@
+
+export const APPColors = {
+    darkBlue: '#0d2e57',
+    lightBlue: '#3f87e0',
+    stTopaz: '#2851A3'
+}
