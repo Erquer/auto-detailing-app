4a105654a3a74d123f42d66d64e8366f4e4b0cf8
Erquer <baryla181@gmail.com>
2021-05-26 01:08:28 +0200

    Add ChartJs to Income Section

--- frontend/src/components/app-container/dashboard/Income/Income.tsx
+++ frontend/src/components/app-container/dashboard/Income/Income.tsx
@@ -1,12 +1,55 @@
 import React from "react";
 import Button from "../../../Button/Button";
+import { Bar } from 'react-chartjs-2';
 
-const Income = () => (
+interface DataProps {
+  date: string;
+  income: number;
+}
+
+const Income = () => {
+  const [data, setData] = React.useState<Array<DataProps>>([]);
+  React.useEffect(() => {
+    const rawData = require('./income.data.json');
+    setData(rawData.incomes);
+    console.log(rawData.incomes);
+  }, []);
+
+  const getData = () =>{
+    return(
+      {
+        labels: data.map(d => d.date),
+        datasets: [{
+          label: 'Incomes',
+          data: data.map(d => d.income),
+          backgroundColor: 'blue',
+          legend: {
+            display: false
+          },
+          title: {
+            display: false
+          }
+        }]
+        
+      }
+    )
+
+  }
+  
+  return (
   <div>
+    <span style={{display:'inline-flex'}}>
     <h2>Przychody</h2>
-    <div>wykres</div>
     <Button innerText="SPRAWDŹ CAŁĄ HISTORIE" to="/orders" />
+    </span>
+    <Bar 
+    height={35}
+    width={150}
+    type='bar'
+    data={getData}
+    />
+    
   </div>
-);
+);}
 
 export default Income;
