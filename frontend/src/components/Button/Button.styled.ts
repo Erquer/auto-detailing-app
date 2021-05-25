9275e1b4017285895e63195ba1978a3c8ca110a8
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-05-26 01:08:07 +0200

    Calendar first iteration

--- frontend/src/components/Button/Button.styled.ts
+++ frontend/src/components/Button/Button.styled.ts
@@ -2,13 +2,13 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const StyledButtonAsLink = styled(Link)`
-  position: absolute;
+  display: block;
   background: #2851a3;
   border-radius: 20px;
   padding: 10px;
   font-size: 12px;
   text-align: center;
   color: #ffffff;
-  min-width: 180px;
+  max-width: 200px;
   text-decoration: none;
 `;
