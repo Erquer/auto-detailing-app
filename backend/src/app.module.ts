e8f653e02fde1e9c577a0d98a9dd77271868a090
Błażej <baryla181@gmail.com>
2021-09-01 22:25:44 +0200

    add example controller: UserController

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database-module/database.module';
+import { UserController } from './database-module/users/user.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AppController],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
